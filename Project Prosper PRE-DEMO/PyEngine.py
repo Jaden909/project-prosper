3e8d15983827cc008ca3e011b66e56c970e82989
Jaden909 <118631735+Jaden909@users.noreply.github.com>
2023-11-08 19:14:01 -0500

    init

---
+++ Project Prosper PRE-DEMO/PyEngine.py
@@ -0,0 +1,335 @@
+"""Game Engine for Python. Requires Pygame.""" 
+import pygame,json,os,sys,importlib
+pygame.init()
+_mouseDown=False
+buttons=[]
+messages=[]
+elements=[]
+
+class Message:
+    "Creates a message that can be sent and recieved"
+    def __init__(self,name):
+        self.name=name
+        message={'name':self.name,'state':False}
+        messages.append(message)
+    def send(self):
+        for message in messages:
+            if message.get('name')==self.name:
+                message.update(state=True)
+    def unsend(self):
+        for message in messages:
+            if message.get('name')==self.name:
+                message.update(state=False)
+    def listen(self):
+        for message in messages:
+            if message.get('state'):
+                return True
+            else:
+                return False
+class GameButton:
+    """Create a button that can trigger a function if clicked on or a message is recieved (if one is provided)"""
+    def __init__(self,x:int=0,y:int=0,function=None,notHoverSprite:pygame.Surface='default',hover:bool=True,active:bool=True,hoverSprite:pygame.Surface='default',imageRes:int=64,image:str='Defaults\\DEFAULTBUTTON.png',imageResX:int=0,imageResY:int=0,message:Message=None,hoverAlt=None):
+        self.hovering=False
+        #REQUIRED ARGS  
+        self.x=x
+        self.y=y    
+        self.imageResX=imageResX
+        self.imageResY=imageResY
+        if imageResX !=0 or imageResY !=0:
+            self.square=False
+        else:
+            self.square=True
+        if image is not None:
+            self.image=pygame.image.load(image).convert()
+        self.imageRes=imageRes
+        self.function=function
+        self.hover=hover
+        self.hoverSprite=hoverSprite
+        self.notHoverSprite=notHoverSprite
+        if hoverSprite=='default':
+            self.hoverCursor=pygame.SYSTEM_CURSOR_HAND
+        elif hoverSprite=='ibeam':
+            self.hoverCursor=pygame.SYSTEM_CURSOR_IBEAM
+        else:
+            self.hoverCursor=pygame.cursors.Cursor((0,0),self.hoverSprite)
+        if notHoverSprite=='default':
+            self.notHoverCursor=pygame.SYSTEM_CURSOR_ARROW   
+        elif notHoverSprite=='ibeam':
+            self.notHoverCursor=pygame.SYSTEM_CURSOR_IBEAM 
+        else:
+            self.notHoverCursor=pygame.cursors.Cursor((0,0),self.notHoverSprite) 
+        self.active=active
+        self.message=message
+        if hoverAlt is not None:
+            self.hoverAlt=pygame.image.load(hoverAlt)
+        else:
+            self.hoverAlt=None
+        buttons.append(self)    
+    #Blits button to screen provided
+    def show(self,screen:pygame.surface.Surface):
+        'Blit button to screen. Screen to blit to required. Requires defined image'
+        screen.blit(self.image,(self.x,self.y))
+    
+    #Listens for mouse clicks: should be in a loop to work properly
+    def listen(self,screen=None):    
+        "Listens for clicks on the button. Should be in a loop."
+        global _mouseDown
+        if self.active:
+            left,middle,right=pygame.mouse.get_pressed()
+            #Hover
+            if self.hover:    
+                if self.square:
+                    mouseX,mouseY=pygame.mouse.get_pos()
+                    if mouseX>self.x and mouseX<self.x+self.imageRes and mouseY>self.y and mouseY<self.y+self.imageRes:
+                        pygame.mouse.set_cursor(self.hoverCursor)
+                        if self.hoverAlt is not None and screen is not None:
+                            screen.blit(self.hoverAlt,(self.x,self.y))
+                        self.hovering=True
+                    elif self.hovering:
+                        pygame.mouse.set_cursor(self.notHoverCursor)
+                        self.hovering=False
+                else:
+                    mouseX,mouseY=pygame.mouse.get_pos()
+                    if mouseX>self.x and mouseX<self.x+self.imageResX and mouseY>self.y and mouseY<self.y+self.imageResY:
+                        pygame.mouse.set_cursor(self.hoverCursor)
+                        if self.hoverAlt is not None and screen is not None:
+                            screen.blit(self.hoverAlt,(self.x,self.y))
+                        self.hovering=True
+                    elif self.hovering:
+                        pygame.mouse.set_cursor(self.notHoverCursor)
+                        self.hovering=False
+            
+            #CLick Event
+            if left and self.function:
+                if self.square:    
+                    mouseX,mouseY=pygame.mouse.get_pos()
+                    if mouseX>self.x and mouseX<self.x+self.imageRes and mouseY>self.y and mouseY<self.y+self.imageRes and _mouseDown==False:
+                        self.function()
+                        _mouseDown=True
+                else:
+                    mouseX,mouseY=pygame.mouse.get_pos()
+                    if mouseX>self.x and mouseX<self.x+self.imageResX and mouseY>self.y and mouseY<self.y+self.imageResY and _mouseDown==False:
+                        self.function()
+                        _mouseDown=True
+            else: _mouseDown=False
+            if self.message and self.function:
+                if self.message.listen():
+                    self.function()
+    def listenPulse(self):
+        "Listens for hover once. Should NOT be used in a loop. Useful for controllers."
+        if self.active and self.function:
+            if self.square:    
+                mouseX,mouseY=pygame.mouse.get_pos()
+                if mouseX>self.x and mouseX<self.x+self.imageRes and mouseY>self.y and mouseY<self.y+self.imageRes:
+                    self.function()
+            else:
+                mouseX,mouseY=pygame.mouse.get_pos()
+                if mouseX>self.x and mouseX<self.x+self.imageResX and mouseY>self.y and mouseY<self.y+self.imageResY:
+                    self.function() 
+    def enable(self):
+        self.active=True
+    def disable(self):
+        self.active=False
+class Vector2:
+    def __init__(self,x:int|float,y:int|float):
+        self.value=(x,y)
+    def translate(self,x=0,y=0):
+        self.x=self.value.__getitem__(0)
+        self.y=self.value.__getitem__(1)
+        self.x+=x
+        self.y+=y
+        self.value=(self.x,self.y)
+class staticImage:
+    "Static Graphic that doesn't move. Requires x,y and image"
+    def __init__(self,x:int,y:int,image:str):
+        self.x=x
+        self.y=y     
+        self.image=pygame.image.load(image).convert()
+    def show(self,screen):
+        screen.blit(self.image,(self.x,self.y))
+class TextBox:
+    def __init__(self,text:str,x:int,y:int,height:int,width:int,fontName:str,size:int,bold:bool,italic:bool,textColor:str|tuple,bgColor:str|tuple|None,borderColor:str|tuple|None,borderWidth:int,textOffset:tuple):
+        self.text=text
+        self.x=x
+        self.y=y
+        self.height=height
+        self.width=width
+        self.textColor=textColor
+        self.rect=pygame.rect.Rect(self.x,self.y,self.width,self.height)
+        self.mask=pygame.Mask((self.width,self.height),True)
+        self.bgColor=bgColor
+        self.borderColor=borderColor
+        self.borderWidth=borderWidth
+        self.textOffset=textOffset
+        self.font=pygame.font.Font(fontName,size)
+        #self.font=pygame.font.SysFont(fontName,size,bold,italic)
+        self.textBox=self.font.render(self.text,False,textColor)
+        if bgColor is not None:
+            self.bg=pygame.Surface((self.width,self.height))
+            self.bg.fill(self.bgColor)
+        else:
+            self.bg=None
+        elements.append(self)
+    def update(self,text:str):
+        'Update the text Displayed by the text box'
+        self.text=text
+        self.textBox=self.font.render(self.text,True,self.textColor)
+    def render(self,screen:pygame.Surface,autoFit=True):
+        if autoFit:
+            self.rect.width=self.font.size(self.text)[0]+self.textOffset[0]*2
+            self.width=self.font.size(self.text)[0]+self.textOffset[0]*2
+            if self.bg is not None:
+                self.bg=pygame.Surface((self.width,self.height))
+                self.bg.fill(self.bgColor)
+                screen.blit(self.bg,self.rect)
+        screen.blit(self.textBox,self.rect.move(self.textOffset))
+        if self.borderColor is not None:
+            pygame.draw.rect(screen,self.borderColor,self.rect,self.borderWidth)
+    def snapToMouse(self,borderX:int):
+        if self.rect.right<borderX:
+            self.rect.bottomleft=pygame.mouse.get_pos()
+        if self.rect.right>borderX:
+            self.rect.bottomright=pygame.mouse.get_pos()
+
+        
+
+
+def animation(moveList:list,frameDelay,screen,x,y,startTime,frame):
+    """Pygame animation. returns current frame, startTime and done. Put this in a loop and supply the returned frame and startTime to work properly."""
+    #frame=0
+    currentTime = pygame.time.get_ticks()
+    start=startTime
+    done=False
+    if currentTime - startTime >= frameDelay:
+        frame+=1
+        startTime = currentTime
+    if frame==len(moveList):
+        frame=0
+        done=True
+    screen.blit(moveList[frame],(x,y))
+    return frame,startTime,done
+
+def wasdInput(WFunction=None,AFunction=None,SFunction=None,DFunction=None):
+    """Simple WASD/arrow keys input listener. Also supports arrow keys. Args are functions to run when respective key is pressed"""
+    keys=pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]or keys[pygame.K_a]and AFunction is not None:
+        AFunction()
+    if keys[pygame.K_RIGHT]or keys[pygame.K_d]and DFunction is not None:
+        DFunction()
+    if keys[pygame.K_DOWN]or keys[pygame.K_s]and SFunction is not None:
+        SFunction()
+    if keys[pygame.K_UP]or keys[pygame.K_w]and WFunction is not None:
+        WFunction()
+def enableAll():
+    "Enables all created buttons"
+    for button in buttons:
+        button.enable()
+def disableAll():
+    "Disables all created buttons"
+    for button in buttons:
+        button.disable()
+def listenAll(screen):
+    "Listen on all active buttons at once"
+    global buttons
+    for button in buttons:
+        if button.active:
+            button.listen(screen)
+def showAll(screen):
+    "Show all active buttons at once"
+    for button in buttons:
+        if button.active:
+            button.show(screen)
+def listenPulseAll():
+    "Listen pulse on all created buttons at once"
+    global buttons
+    for button in buttons:
+        button.listenPulse()
+def save(saveFile:str,save):
+    "Save a dictionary of variables to a json file"
+    _save=save
+    with open(saveFile,'w') as f:
+        json.dump(_save,f)
+def load(saveFile:str)-> dict|list:
+    "Load a dictionary of variables from a json file and return it"
+    _save=json.load(open(saveFile))
+    return _save
+def checkHover(x1:int,x2:int,y1:int,y2:int,function):
+    "Runs a function if mouse is in given area"
+    mouseX,mouseY=pygame.mouse.get_pos()
+    if mouseX>x1 and mouseX<x2 and mouseY>y1 and mouseY<y2:
+        function()
+def loadMods(modDir:str,loadingScreen:pygame.Surface=None,screen:pygame.Surface=None)->list:
+    "Load mods in given directory. Returns list of loaded mods as Module objects. The folder containing each mod's files must start with an uppercase letter and the .py file must be the same name but lowercase."
+    loadedMods=[]
+    logList=[]
+    log={}
+    class Mod:
+        def __init__(self,name,title,description,version,id,author,modified,script,icon):
+            self.name=name
+            self.title=title
+            self.description=description
+            self.version=version
+            self.id=id
+            self.author=author
+            self.modified=modified
+            self.script=script
+            self.icon=icon
+        def loop(self):
+            self.script.loop()
+        def init(self):
+            self.script.init()
+        def config(self):
+            self.script.config() 
+    for root, dirs, files in os.walk(f'{modDir}'):
+            for name in dirs:
+                if name[0].isupper():
+                    
+                    print('________________________________________________________________________________________')
+                    logList.append('________________________________________________________________________________________')
+                    print(f'Mod named {name} found.')
+                    logList.append(f'Mod named {name} found.')
+                    sys.path.insert(1,f'{modDir}\\{name}')
+                    print(f'Folder named {name} successfully added to sys.path.')
+                    logList.append(f'Folder named {name} successfully added to sys.path.')
+                    meta=load(f'{modDir}\\{name}\\meta.json')
+                    print(f'meta data of {name} successfully loaded.')
+                    logList.append(f'meta data of {name} successfully loaded.')
+                    mod=Mod(name,meta.get('title'),meta.get('description'),meta.get('version'),meta.get('id'),meta.get('author'),meta.get('modified'),importlib.import_module(name.lower()),None)
+                    print(f'Mod object successfully created using the meta data of {name}.')
+                    logList.append(f'Mod object successfully created using the meta data of {name}.')
+                    loadedMods.append(mod)
+                    print(f'{name}\'s mod object succesfully added to list of mods.')
+                    print(f'Mod named {name} successfully loaded.')
+                    logList.append(f'{name}\'s mod object succesfully added to list of mods.')
+                    logList.append(f'Mod named {name} successfully loaded.')
+                    if loadingScreen is not None:
+                        screen.blit(loadingScreen,(0,0))    
+    log.update(log=logList)
+    save('log.json',log)
+    print('Log saved to log.json file.')
+    return loadedMods
+def renderUI():
+    for element in elements:
+        element.render()
+def autoWrap(text:str,width:int,font:pygame.font.Font,textColor):
+    "returns a list of surfaces of text autowrapped to a given width"
+    line=''
+    lines=[]
+    words=text.split(' ')
+    for i in range(len(words)):
+        if font.size(line+words[i]+' ')[0]<=width:
+            line+=words[i]+' '
+        else:
+            lines.append(font.render(line,True,textColor))
+            line=''
+            line+=words[i]+' '
+            i-=2
+    if line!='':
+        lines.append(font.render(line,True,textColor))
+    return lines
+#print(autoWrap('This is an incredible test i sure hope it doesn\'t go wrong',170,pygame.font.SysFont('comic sans ms',12),'white'))
+
+if __name__=='__main__':
+    print('This script doesn\'t work on its own. Import it into a project to use the functions and classes defined here')
+else:
+    print('Using PyEngine v0.4 APLHA') 
---
+++ Project Prosper PRE-DEMO/itemTool.py
@@ -0,0 +1,150 @@
+import json
+from customtkinter import *
+from tkinter import *
+itemType=None
+def create(type):
+    global itemType,itemName,itemSprite,blockSprite,maxStackSize
+    itemType=tab.get()
+    if type=='item':
+        items.append({'Name':itemName.get(),'Id':len(items),'MaxStackSize':int(maxStackSize.get()),'Sprite':itemSprite.get(),'Type':itemType})
+        idInd.configure(True,text=f'This item\'s id will be: {len(items)}')
+        print(items)
+    elif type=='block':
+        blocks.append({'Name':itemName.get(),'Id':len(items),'MaxStackSize':int(maxStackSize.get()),'Sprite':itemSprite.get(),'BlockSprite':blockSprite.get(),'Type':itemType})
+        idInd.configure(True,text=f'This item\'s id will be: {len(items)}')
+        print(items)
+    elif type=='tool':
+        #items.append({'Name':itemName.get(),'Id':len(items),'Sprite':itemSprite.get(),'Type':itemType})
+        idInd.configure(True,text=f'This item\'s id will be: {len(items)}')
+        print(items)
+    elif type=='recipe':
+        print('WIP DO NOT USE')
+        for slot in recipe:
+            if slot.get()=='':
+                realRecipe.append(None)
+            else:
+                realRecipe.append(int(slot.get()))
+        recipes.append({"Id":len(recipes),"Name":recipeName.get(),"Recipe":realRecipe,"Output":int(output.get()),"Shapeless":shapeless.get(),"Count":int(count.get())})
+        recIdInd.configure(True,text=f'This recipe\'s id will be: {len(recipes)}')
+        print(recipes)
+def saveItem():
+    with open('items.json','w') as f:
+        json.dump(items,f)
+def saveBlock():
+    with open('blocks.json','w') as f:
+        json.dump(blocks,f)
+def saveRecipe():
+    with open('recipes.json','w') as f:
+        json.dump(recipes,f)
+def itemList():
+    itemListWin=CTk()
+    itemListWin.title('Item List')
+    itemListWin.geometry('200x400')
+    CTkLabel(itemListWin,text='Name').grid(column=1,row=0)
+    CTkLabel(itemListWin,text='Icon').grid(column=2,row=0,padx=30)
+    CTkLabel(itemListWin,text='Id').grid(column=3,row=0)
+    for item in items:
+        #print('items\\'+item['Sprite'])
+        CTkLabel(itemListWin,text=item['Name'],anchor='w').grid(column=1,row=item['Id']+1)
+        CTkLabel(itemListWin,image=PhotoImage(master=itemListWin,file='items\\'+item['Sprite']),text=None,anchor='w').grid(column=2,row=item['Id']+1)
+        CTkLabel(itemListWin,text=item['Id'],anchor='w').grid(column=3,row=item['Id']+1)
+    itemListWin.mainloop()
+mainWin=CTk()
+mainWin.title('Item Creation Tool')
+mainWin.geometry('400x450')
+items:list=json.load(open('items.json'))
+blocks=json.load(open('blocks.json'))
+recipes=json.load(open('recipes.json'))
+with open('itemsBackup.json','w') as f:
+    json.dump(items,f)
+with open('blocksBackup.json','w') as f:
+    json.dump(blocks,f)
+with open('recipesBackup.json','w') as f:
+    json.dump(recipes,f)
+#SegmentedButton an TabView
+realRecipe=[]
+itemName=StringVar()
+recipeName=StringVar()
+maxStackSize=StringVar()
+itemSprite=StringVar()
+blockSprite=StringVar()
+output=StringVar()
+shapeless=BooleanVar()
+count=StringVar()
+recipe=[StringVar()for i in range(9)]
+#print(recipe)
+typeLabel=CTkLabel(mainWin,text='Item Type').pack()
+tab=CTkTabview(mainWin,height=0)
+tab.add('Item')
+tab.add('Block')
+tab.add('Tool')
+tab.add('Recipe')
+#Name Label
+CTkLabel(tab.tab('Item'),text='Item Name').pack()
+CTkLabel(tab.tab('Block'),text='Block Name').pack()
+CTkLabel(tab.tab('Tool'),text='Tool Name').pack()
+CTkLabel(tab.tab('Recipe'),text='Recipe Name').pack()
+#Name Entry (1)
+CTkEntry(tab.tab('Item'),textvariable=itemName).pack()
+CTkEntry(tab.tab('Block'),textvariable=itemName).pack()
+CTkEntry(tab.tab('Tool'),textvariable=itemName).pack()
+CTkEntry(tab.tab('Recipe'),textvariable=recipeName).pack()
+#Max Stack Size Label
+CTkLabel(tab.tab('Item'),text='Max Stack Size').pack()
+CTkLabel(tab.tab('Block'),text='Max Stack Size').pack()
+#Max Stack Size Entry(2)
+CTkEntry(tab.tab('Item'),textvariable=maxStackSize).pack()
+CTkEntry(tab.tab('Block'),textvariable=maxStackSize).pack()
+#Recipe Label
+CTkLabel(tab.tab('Recipe'),text='Recipe (enter the ids of the component items and the output)').pack()
+#Recipe Entries(2)
+recFrame=CTkFrame(tab.tab('Recipe'))
+CTkEntry(recFrame,textvariable=recipe[0],width=20).grid(row=1,column=1)
+CTkEntry(recFrame,textvariable=recipe[1],width=20).grid(row=1,column=2)
+CTkEntry(recFrame,textvariable=recipe[2],width=20).grid(row=1,column=3)
+CTkEntry(recFrame,textvariable=recipe[3],width=20).grid(row=2,column=1)
+CTkEntry(recFrame,textvariable=recipe[4],width=20).grid(row=2,column=2)
+CTkEntry(recFrame,textvariable=recipe[5],width=20).grid(row=2,column=3)
+CTkEntry(recFrame,textvariable=recipe[6],width=20).grid(row=3,column=1)
+CTkEntry(recFrame,textvariable=recipe[7],width=20).grid(row=3,column=2)
+CTkEntry(recFrame,textvariable=recipe[8],width=20).grid(row=3,column=3)
+
+CTkEntry(recFrame,textvariable=output,width=20).grid(row=2,column=5)
+recFrame.pack()
+#Sprite Label
+CTkLabel(tab.tab('Item'),text='Item Sprite Location').pack()
+CTkLabel(tab.tab('Block'),text='Item Sprite Location').pack()
+CTkLabel(tab.tab('Tool'),text='Item Sprite Location').pack()
+#Sprite Entry (3)
+CTkEntry(tab.tab('Item'),textvariable=itemSprite).pack()
+CTkEntry(tab.tab('Block'),textvariable=itemSprite).pack()
+CTkEntry(tab.tab('Tool'),textvariable=itemSprite).pack()
+#Shapeless Label
+#CTkLabel(tab.tab('Recipe'),text='Is the recipe shapeless?').pack()
+#Shapeless Switch(3)
+CTkSwitch(tab.tab('Recipe'),text='Is the recipe shapeless?',variable=shapeless).pack()
+#Block Sprite Label
+CTkLabel(tab.tab('Block'),text='Placed Sprite Location').pack()
+#Count Label
+CTkLabel(tab.tab('Recipe'),text='How many output items are produced from this recipe?').pack()
+#Count Entry
+CTkEntry(tab.tab('Recipe'),textvariable=count).pack()
+#Block Sprite Entry (4)
+CTkEntry(tab.tab('Block'),textvariable=blockSprite).pack()
+#Create Button
+CTkButton(tab.tab('Item'),text='Create',command=lambda:create('item')).pack()
+CTkButton(tab.tab('Block'),text='Create',command=lambda:create('block')).pack()
+CTkButton(tab.tab('Tool'),text='Create',command=lambda:create('tool')).pack()
+CTkButton(tab.tab('Recipe'),text='Create',command=lambda:create('recipe')).pack()
+#Id Indicator
+idInd=CTkLabel(tab.tab('Item'),text=f'This item\'s id will be: {len(items)}')
+idInd.pack()
+recIdInd=CTkLabel(tab.tab('Recipe'),text=f'This recipe\'s id will be: {len(recipes)}')
+recIdInd.pack()
+#Save Button
+CTkButton(tab.tab('Item'),text='Save to JSON',command=saveItem).pack()
+CTkButton(tab.tab('Block'),text='Save to JSON',command=saveBlock).pack()
+CTkButton(tab.tab('Recipe'),text='Save to JSON',command=saveRecipe).pack()
+tab.pack()
+CTkButton(mainWin,text='View all items',command=itemList).pack()
+mainWin.mainloop()
